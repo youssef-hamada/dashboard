9bd49c99d6b3d15d724d9d8fd99b547f20b7e258
unknown <hamaday468@gmail.com>
2025-01-11 20:37:18 +0200

    start with login pagr

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "@/app/ui/login/login.module.css";
 
 const LoginPage = () => {
-  return <div>loginnpm </div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="Email" className={styles.email} />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.password}
+        />
+        <button className={styles.button}>login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
